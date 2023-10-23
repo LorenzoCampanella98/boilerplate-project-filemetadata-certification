b8fcd932ff0f759f4a9655ce315170026e299433
LorenzoCampanella98 <84874986+LorenzoCampanella98@users.noreply.github.com>
2023-10-23 15:20:42 +0000

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +8,28 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 
+// -------------------- SOLUTION ---------------------
+
+app.post('/api/fileanalyse', multer().single('upfile'),(req, res) => {
+  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
+// ---------------------------------------------------
+
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
